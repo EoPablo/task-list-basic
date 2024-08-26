3dc982384e9716171d14949d259d3d6229b93639
Pablo <103059793+EoPablo@users.noreply.github.com>
2024-08-26 15:53:42 -0300

    Projeto Completo

---
+++ script.js
@@ -0,0 +1,63 @@
+// Elementos
+
+let listElement = document.querySelector('#app ul');
+let inputElement = document.querySelector('#app input');
+let buttonElement = document.querySelector('#app button');
+
+let tarefas = JSON.parse(localStorage.getItem('@listaTarefas')) || [] ;
+
+function mostrarTarefas() {
+    listElement.innerHTML = '';
+
+   tarefas.map((tarefa) => {
+    let liElement = document.createElement('li');
+    let tarefaText = document.createTextNode(tarefa);
+
+    let linkElement = document.createElement('a');
+    linkElement.setAttribute('href', '#');
+
+    let linkText = document.createTextNode('Excluir');
+    linkElement.appendChild(linkText);
+
+    let posicao = tarefas.indexOf(tarefa);
+    
+    linkElement.setAttribute('onclick', `deletarTarefa(${posicao})`);
+
+
+    liElement.appendChild(tarefaText);
+    liElement.appendChild(linkElement);
+    listElement.appendChild(liElement);
+   }) 
+}
+
+mostrarTarefas();
+
+function adicionarTarefas() {
+    if(inputElement.value === '') {
+       alert('Digite alguma tarefa');
+       return false; 
+    } else {
+        let novaTarefa = inputElement.value;
+
+        tarefas.push(novaTarefa);
+        inputElement.value = '';
+
+        mostrarTarefas();
+        salvarDados();
+    }
+
+}
+
+buttonElement.onclick = adicionarTarefas;
+
+function deletarTarefa(posicao) {
+    tarefas.splice(posicao, 1);
+    mostrarTarefas();
+    salvarDados();
+}
+
+// SALVAR NO LOCALSTORAGE
+
+function salvarDados() {
+    localStorage.setItem('@listaTarefas', JSON.stringify(tarefas))
+}
